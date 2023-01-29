b8cf971f65f7e58be88077df45f0d69a7b42145f
Petr Popov <91988704+wswerlu@users.noreply.github.com>
2023-01-29 15:27:42 +0300

    Add schema validation to test create booking success (#3)

--- utils/helpers.py
+++ utils/helpers.py
@@ -1,7 +1,14 @@
 from glob import glob
+from json import load
 from os.path import join
 from pathlib import Path
 from platform import system
+
+from allure import step
+from jsonschema.exceptions import ValidationError
+from jsonschema.validators import validate
+from mimesis.exceptions import SchemaError
+from requests import Response
 
 
 def get_fixtures():
@@ -13,3 +20,29 @@ def get_fixtures():
         if file not in ['__init__', '__pycache__']:
             file_path.append(f'fixtures.{file}')
     return file_path
+
+
+def validate_json(response: Response, name: str):
+    with open(Path(__file__).parent.parent / 'json_schema' / f'{name}.json') as file:
+        json_file = load(file)
+    try:
+        validate(instance=response, schema=json_file)
+        return True
+    except SchemaError:
+        print('Схема содержит ошибку')
+    except ValidationError as e:
+        print('Ошибка', e)
+    except Exception as e:
+        print(e)
+    return False
+
+
+@step('Проверка ответа API - {name}')
+def json_schema_asserts(response: Response, name: str):
+    """
+    Метод проверки достоверности результата теста на основе схемы json
+
+    :param response: Ответ от сервера
+    :param name: Имя схемы JSON
+    """
+    assert validate_json(response, name), f'Ошибка при валидации схемы - {name}'
