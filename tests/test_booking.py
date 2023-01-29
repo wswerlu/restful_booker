83395a0f73b705e3bb43ee6ec8a0f9dad65e9982
Petr Popov <popovpn7@gmail.com>
2023-01-29 15:25:41 +0300

    Add schema validation to test: test_create_booking_success

--- tests/test_booking.py
+++ tests/test_booking.py
@@ -1,5 +1,7 @@
 from allure import epic, feature, title
 
+from utils.helpers import json_schema_asserts
+
 
 @epic('Api')
 @feature('Бронирование')
@@ -11,6 +13,7 @@ class TestBooking:
     @title('Успешное создание бронирования')
     def test_create_booking_success(self, booking_api):
         booking = booking_api.create_booking()
+        json_schema_asserts(response=booking, name='create_booking')
         booking_id = booking['bookingid']
         booking_api.should_be_booking_with_id(
             booking_id=booking_id,
