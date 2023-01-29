e9abc244abdaa18cc0ba2c23c627626cfe0c40c8
Petr Popov <popovpn7@gmail.com>
2023-01-29 17:44:55 +0300

    Add test: test_update_booking_success

--- tests/test_booking.py
+++ tests/test_booking.py
@@ -1,5 +1,9 @@
+from datetime import date, timedelta
+from random import choice, randint
+
 from allure import epic, feature, title
 
+from utils.generated_test_data import UserData
 from utils.helpers import json_schema_asserts
 
 
@@ -13,10 +17,44 @@ class TestBooking:
     @title('Успешное создание бронирования')
     def test_create_booking_success(self, booking_api):
         booking = booking_api.create_booking()
+
         json_schema_asserts(response=booking, name='create_booking')
+
         booking_id = booking['bookingid']
         booking_api.should_be_booking_with_id(
             booking_id=booking_id,
             firstname=booking['booking']['firstname'],
             lastname=booking['booking']['lastname'],
+        )
+
+    @title('Успешное обновление бронирования')
+    def test_update_booking_success(self, booking_api, create_booking):
+        checkin = date.today() + timedelta(days=randint(0, 366))
+        checkout = checkin + timedelta(days=randint(0, 366))
+        booking = {
+            'firstname': UserData().firstname(),
+            'lastname': UserData().lastname(),
+            'totalprice': randint(1, 100000),
+            'depositpaid': choice([True, False]),
+            'checkin': str(checkin),
+            'checkout': str(checkout),
+            'additionalneeds': choice(['Breakfast', 'Lunch', 'Dinner']),
+        }
+
+        booking_id = create_booking['bookingid']
+        update_booking = booking_api.update_booking(
+            booking_id=booking_id,
+            firstname=booking['firstname'],
+            lastname=booking['lastname'],
+            total_price=booking['totalprice'],
+            deposit_paid=booking['depositpaid'],
+            checkin=booking['checkin'],
+            checkout=booking['checkout'],
+            additional_needs=booking['additionalneeds'],
+        )
+
+        json_schema_asserts(response=update_booking, name='update_booking')
+        booking_api.should_be_updated_booking(
+            booking_id=booking_id,
+            expected_booking_info=booking,
         )
